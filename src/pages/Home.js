7098e126e4ae081e630234674a248201538546cc
SterlingVel <sterlin.velazquez37@gmail.com>
2023-11-08 13:06:54 -0500

    Fixed lazy loading issue and fade-in bugs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import '../css/index.css';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion'
 import Fade from '../tools/fade.js'
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const pageVariants = {
     in: {
@@ -32,6 +33,7 @@ class Home extends React.Component {
     componentDidMount() {
         window.scrollTo(0, 0);
         this.addResizeListener();
+        document.getElementById("navmenu").className = "navMenu"
     }
 
     render() {
@@ -39,12 +41,12 @@ class Home extends React.Component {
             <motion.div className="main" initial="out" animate="in" exit="out" variants={pageVariants}>
                 <div className="introDiv">
                     <div className="introHeaderSection">
-                        <img className="introImageButterfly" id="introimagebutterfly" alt="butterfly" src="introImages/butterfly.png"></img>
+                        <img className="introImageButterfly" id="introimagebutterfly" alt="butterfly" src="introImages/butterfly.png"/>
                         <p className="introTitle">Sterling + Alanna</p>
                         <p className="introSubHeader">THE VELAZQUEZ WEDDING</p>
                         <p className="introDate">April 6th, 2024 | 5:30 pm</p>
                     </div>
-                    <img className="introImageFlower" id="introimageflower" alt="flowers" src="introImages/flowers.png"></img>
+                    <img className="introImageFlower" id="introimageflower" alt="flowers" src="introImages/flowers.png"/>
                     <div className="introFade"></div>
                 </div>
 
@@ -57,14 +59,14 @@ class Home extends React.Component {
                                         <p className="homeGalleryText">WEDDING</p>
                                     </Link>
                                     <Link to='/wedding'>
-                                        <img className="homeGalleryPic" alt="weddingmenu" src="introImages/twirl.jpg" loading="lazy" />
+                                        <LazyLoadImage className="homeGalleryPic" alt="weddingmenu" src="introImages/twirl.jpg" loading="lazy" />
                                     </Link>
                                 </div>
                             </Fade>
                             <Fade id="travellink">
                                 <div className="homeGallerySection travel">
                                     <Link to='/wedding'>
-                                        <img className="homeGalleryPic" alt="weddingmenu" src="introImages/field.jpg" loading="lazy" />
+                                        <LazyLoadImage className="homeGalleryPic" alt="weddingmenu" src="introImages/field.jpg" loading="lazy" />
                                     </Link>
                                     <Link to='/wedding'>
                                         <p className="homeGalleryText">TRAVEL</p>
@@ -82,14 +84,14 @@ class Home extends React.Component {
                                         <p className="homeGalleryText">OUR STORY</p>
                                     </Link>
                                     <Link to='/wedding'>
-                                        <img className="homeGalleryPic" alt="weddingmenu" src="introImages/ring.jpg" loading="lazy" />
+                                        <LazyLoadImage className="homeGalleryPic" alt="weddingmenu" src="introImages/ring.jpg" loading="lazy" />
                                     </Link>
                                 </Fade>
                             </div>
                             <Fade id="giftslink">
                                 <div className="homeGallerySection gifts">
                                     <Link to='/wedding'>
-                                        <img className="homeGalleryPic" alt="weddingmenu" src="introImages/arms.jpg" loading="lazy" />
+                                        <LazyLoadImage className="homeGalleryPic" alt="weddingmenu" src="introImages/arms.jpg" loading="lazy" />
                                     </Link>
                                     <Link to='/wedding'>
                                         <p className="homeGalleryText">GIFTS</p>
@@ -103,17 +105,17 @@ class Home extends React.Component {
                     </div>
                     <div className="galleryFlower left">
                         <Fade id="homeflowerleft" threshold="0.2">
-                            <img className="flowerImage" alt="flower" src="introImages/homeflowers.png" loading="lazy"></img>
+                            <LazyLoadImage className="flowerImage" alt="flower" src="introImages/homeflowers.png" loading="lazy"/>
                         </Fade>
                     </div>
                     <div className="galleryFlower right1">
                         <Fade id="homeflowerright1" threshold="0.1">
-                            <img className="flowerImage" alt="flower" src="introImages/homeflowers2.png" loading="lazy"></img>
+                            <LazyLoadImage className="flowerImage" alt="flower" src="introImages/homeflowers2.png" loading="lazy"/>
                         </Fade>
                     </div>
                     <div className="galleryFlower right2">
                         <Fade id="homeflowerright2" threshold="0.1">
-                            <img className="flowerImage" alt="flower" src="introImages/homeflowers.png" loading="lazy"></img>
+                            <LazyLoadImage className="flowerImage" alt="flower" src="introImages/homeflowers.png" loading="lazy"/>
                         </Fade>
                     </div>
                     <div className="homeContainer fade"></div>
--- src/pages/Wedding.js
+++ src/pages/Wedding.js
@@ -6,6 +6,7 @@ import '../css/index.css';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion'
 import Fade from '../tools/fade.js'
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const pageVariants = {
     in: {
@@ -32,6 +33,7 @@ class Wedding extends React.Component {
     componentDidMount() {
         window.scrollTo(0, 0);
         this.addResizeListener();
+        document.getElementById("navmenu").className = "navMenu"
     }
 
     render() {
@@ -42,16 +44,16 @@ class Wedding extends React.Component {
                         <p className="weddingSubHeader">WELCOME TO OUR</p>
                         <p className="weddingTitle">Wedding Celebration</p>
                     </div>
-                    <img className="weddingImageFlower left1" alt="flowers" src="weddingImages/b7.png"></img>
-                    <img className="weddingImageFlower left2" alt="flowers" src="weddingImages/1.PNG"></img>
-                    <img className="weddingImageFlower right" alt="flowers" src="weddingImages/bouquet_top.png"></img>
+                    <img className="weddingImageFlower left1" alt="flowers" src="weddingImages/b7.png"/>
+                    <img className="weddingImageFlower left2" alt="flowers" src="weddingImages/1.PNG"/>
+                    <img className="weddingImageFlower right" alt="flowers" src="weddingImages/bouquet_top.png"/>
                 </div>
 
                 <div className="hazlehurstDiv">
                     <Fade threshold="0.6">
                         <div className="hazlehurstImageWrapper">
                             <div className="hazlehurstFade"></div>
-                            <img className='hazlehurstImage' src="weddingImages/hazelhurst-house.jpg" alt="house" loading="lazy"></img>
+                            <LazyLoadImage className='hazlehurstImage' src="weddingImages/hazelhurst-house.jpg" alt="house" loading="lazy"/>
                             <div className="hazlehurstFade"></div>
                         </div>
                     </Fade>
@@ -99,12 +101,12 @@ class Wedding extends React.Component {
                         </div>
                         <div className="scheduleFlowerWrapper">
                             <Fade threshold="0" id="scheduleflower">
-                                <img className="scheduleFlower" alt="flower" src="weddingImages/24.PNG" loading="lazy"></img>
+                                <LazyLoadImage className="scheduleFlower" alt="flower" src="weddingImages/24.PNG" loading="lazy"/>
                             </Fade>
                         </div>
                         <div className="scheduleButterflyWrapper">
-                            <Fade threshold="1" id="schedulebutterfly">
-                                <img className="scheduleButterfly" alt="butterfly" src="weddingImages/32.PNG" loading="lazy"></img>
+                            <Fade threshold="0.9" id="schedulebutterfly">
+                                <LazyLoadImage className="scheduleButterfly" alt="butterfly" src="weddingImages/32.PNG" loading="lazy"/>
                             </Fade>
                         </div>
                     </div>
@@ -121,7 +123,7 @@ class Wedding extends React.Component {
                     </div>
                     <div className="crewHeader bride">
                         <Fade id="crewheaderbrideflower" threshold="0.6">
-                            <img className="brideFlower" alt="flower" src="weddingImages/2.PNG" loading="lazy"></img>
+                            <LazyLoadImage className="brideFlower" alt="flower" src="weddingImages/2.PNG" loading="lazy"/>
                         </Fade>
                         <Fade id="crewheaderbridetext" threshold="0">
                             <p className="crewHeaderText bride">For the Bride</p>
@@ -130,14 +132,14 @@ class Wedding extends React.Component {
                     <div className="crewRow">
                         <Fade>
                             <div className="crewSection">
-                                <img className="crewImage" alt="bridesmaid" src="weddingImages/placeholder.jpg" loading="lazy"></img>
+                                <LazyLoadImage className="crewImage" alt="bridesmaid" src="weddingImages/placeholder.jpg" loading="lazy"/>
                                 <div className="crewSectionText">
                                     <p className="crewSectionHeader">Keeley Cauble</p>
                                     <p className="crewSectionSubheader">Maid of Honor</p>
                                 </div>
                             </div>
                             <div className="crewSection">
-                                <img className="crewImage" alt="bridesmaid" src="weddingImages/placeholder.jpg" loading="lazy"></img>
+                                <LazyLoadImage className="crewImage" alt="bridesmaid" src="weddingImages/placeholder.jpg" loading="lazy"/>
                                 <div className="crewSectionText">
                                     <p className="crewSectionHeader">Amelia Adkisson</p>
                                     <p className="crewSectionSubheader">Bridesmaid</p>
@@ -147,7 +149,7 @@ class Wedding extends React.Component {
                     </div>
                     <div className="crewHeader groom">
                         <Fade id="crewheadergroomflower" threshold="0.2">
-                            <img className="groomFlower" alt="flower" src="weddingImages/14.PNG" loading="lazy"></img>
+                            <LazyLoadImage className="groomFlower" alt="flower" src="weddingImages/14.PNG" loading="lazy"/>
                         </Fade>
                         <Fade id="crewheadergroomtext" threshold="0">
                             <p className="crewHeaderText groom">For the Groom</p>
@@ -156,14 +158,14 @@ class Wedding extends React.Component {
                     <div className="crewRow groom">
                         <Fade>
                             <div className="crewSection">
-                                <img className="crewImage" alt="groomsman" src="weddingImages/placeholder.jpg" loading="lazy"></img>
+                                <LazyLoadImage className="crewImage" alt="groomsman" src="weddingImages/placeholder.jpg" loading="lazy"/>
                                 <div className="crewSectionText">
                                     <p className="crewSectionHeader">Gabriel Torres</p>
                                     <p className="crewSectionSubheader">Best Man</p>
                                 </div>
                             </div>
                             <div className="crewSection">
-                                <img className="crewImage" alt="groomsman" src="weddingImages/placeholder.jpg" loading="lazy"></img>
+                                <LazyLoadImage className="crewImage" alt="groomsman" src="weddingImages/placeholder.jpg" loading="lazy"/>
                                 <div className="crewSectionText">
                                     <p className="crewSectionHeader">Mike Veloso</p>
                                     <p className="crewSectionSubheader">Groomsman</p>
@@ -172,7 +174,7 @@ class Wedding extends React.Component {
                         </Fade>
                     </div>
                     <Fade id="crewflowerend" threshold="0.5">
-                        <img className="crewFlower" alt="flower" src="weddingImages/bouquet.png" loading="lazy"></img>
+                        <LazyLoadImage className="crewFlower" alt="flower" src="weddingImages/bouquet.png" loading="lazy"/>
                     </Fade>
                 </div>
             </motion.div>
