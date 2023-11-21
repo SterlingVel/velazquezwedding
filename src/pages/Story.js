e67845875722c02d2709d2f2e4813d16d8ed70d6
SterlingVel <sterlin.velazquez37@gmail.com>
2023-11-21 17:08:32 -0500

    Removed story paragraphs

--- src/pages/Story.js
+++ src/pages/Story.js
@@ -39,23 +39,7 @@ class Story extends React.Component {
         return (
             <motion.div className="main" initial="out" animate="in" exit="out" variants={pageVariants}>
                 <div className="storyDiv">
-                    This story begins on February 28th, 2018, when two kids who had just started college began exchanging messages
-                    on an online dating app. Neither had expected much out of the online experience, but it wasn't long before they 
-                    had connected and were spending late nights sharing their life stories and growing closer with each passing day.
 
-                    There was Alanna, a girl born and raised in McDonough, Georgia who had a strong desire to leave her small town and
-                    explore what the world had to offer. And there was Sterling, a boy raised in Miami, Florida who had immigrated from Cuba
-                    as a baby with his family and had only recently moved to Atlanta. Feeling as though they were missing something from
-                    their lives, their paths had finally crossed after bridging this gap of hundreds of miles over two decades.
-
-                    Following weeks of conversation, they decided to take the plunge and meet in Alanna's hometown. The nervous excitement
-                    of that first date was quickly replaced by the realization that they had found what was missing. What had started as 
-                    another online connection had transformed into a real-world romance, marked by a mutual desire to explore new places
-                    and have someone special to share the experience with.
-
-                    They made it official on April 6th, 2018. Over the next several years, their bond only strengthened until they could
-                    hardly envision their lives before they met. Now, six years later on April 6th, 2024, they stand on the precipice of a 
-                    new chapter.  
                 </div>
             </motion.div>
         );
